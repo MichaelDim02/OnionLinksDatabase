a8eb59018cfb355a1bf8cf1e73976eb45a047e45
Michael Constantine Dimopoulos <31562759+MichaelDim02@users.noreply.github.com>
2017-11-30 14:02:56 +0000

    Update command.sql

--- command.sql
+++ command.sql
@@ -1,1 +1,8 @@
+-- OLD v0.1
 SELECT ALL FROM links WHERE tag1 = tag OR tag2 = tag OR tag3 = tag;
+-- tag1, tag2, tag3 are the tag columns in the table links of the database
+-- tag is the argument passed into the function (in the python script)
+
+-- This is just a demonstration of the SQL code in case you are interested 
+-- It is not linked with the program
+-- It can be deleted / everything will work correctly without it
